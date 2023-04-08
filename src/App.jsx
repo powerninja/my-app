d77d2a8b027e22bef77307b56f3f31b25acc356b
powerninja <asdyuy23@gmail.com>
2023-04-08 11:15:41 +0900

    propsで受け渡しを行う場合

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,17 @@
-import { ColoredMessage } from "./components/ColorMessage";
-import { CssModules } from "./CssModules";
 import { useState } from "react";
+import { Card } from "./components/Card";
 
 export const App = () => {
-  console.log("レンダリング");
-  //Stateの定義
-  const [num, setNum] = useState(0);
-  const onClickButton = () => {
-    alert("ボタンが押されました");
-    setNum((p) => p + 1);
-  };
+  //管理者フラグ
+  const [isAdmin, setIsAdmin] = useState(false);
 
+  //[切り替え]押下時
+  const onClickSwitch = () => setIsAdmin(!isAdmin);
   return (
-    <>
-      {console.log("test")}
-      <h1 style={{ color: "red" }}>Hello,world</h1>
-      {/* <ColoredMessage color="blue" message="お元気ですか？" fontsize="60px" /> */}
-      <ColoredMessage color="blue">お元気ですか？</ColoredMessage>
-      {/* <ColoredMessage color="pink" message="元気です" fontsize="20px" /> */}
-      <ColoredMessage color="pink">元気です</ColoredMessage>
-      <button onClick={onClickButton}>ボタン</button>
-      <p>{num}</p>
-
-      <CssModules></CssModules>
-    </>
+    <div>
+      {isAdmin ? <span>管理者です</span> : <span>管理者以外です</span>}
+      <button onClick={onClickSwitch}>切り替え</button>
+      <Card isAdmin={isAdmin} />
+    </div>
   );
 };

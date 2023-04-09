ac2ff3a3b5a8e42a63ceecb47abe35c723a99c02
powerninja <asdyuy23@gmail.com>
2023-04-09 21:05:30 +0900

    グローバルに管理する値を参照する(サンプルの固定値)

--- src/components/EditButton.jsx
+++ src/components/EditButton.jsx
@@ -1,3 +1,8 @@
+// reactからusecontextをimport
+import { useContext } from "react";
+
+//作成したContextをimport
+import { AdminFlagContext } from "./providers/AdminFlagProvider";
 const style = {
   width: "100px",
   padding: "6px",
@@ -6,6 +11,11 @@ const style = {
 
 export const EditButton = (props) => {
   const { isAdmin } = props;
+
+  // useContextの引数に参照するContextを指定する
+  const contextValue = useContext(AdminFlagContext);
+  //sampleValue: "テスト"
+  console.log(contextValue);
 
   return (
     <button style={style} disabled={isAdmin}>
